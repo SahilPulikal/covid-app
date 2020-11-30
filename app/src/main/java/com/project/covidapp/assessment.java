d5f9174fe64714a9688e28dc78b824b5b9452283
Sahil Pulikal <sahilpulikal@gmail.com>
2020-12-01 00:58:35 +0530

    added assessment test feature

--- app/src/main/java/com/project/covidapp/assessment.java
+++ app/src/main/java/com/project/covidapp/assessment.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -15,14 +16,30 @@ public class assessment extends AppCompatActivity {
         setContentView(R.layout.activity_assessment);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://google.com");
+        myWebView.loadUrl("https://landing.google.com/screener/covid19");
 
-        Context activityContext = null;
-        myWebView = new WebView(activityContext);
-        setContentView(myWebView);
+//        Context activityContext = null;
+//        myWebView = new WebView(activityContext);
+//        setContentView(myWebView);
 
         myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+//            switch (keyCode) {
+//                case KeyEvent.KEYCODE_BACK:
+//                    if (WebView.canGoBack()) {
+//                        WebView.goBack();
+//                    } else {
+//                        finish();
+//                    }
+//                    return true;
+//            }
+//
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
 }
